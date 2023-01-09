a9992b2c45f0d46d4eb458533776ac51a15e2d59
Alexandre Kalatzis <alpha14@users.noreply.github.com>
2023-01-09 23:14:59 +0100

    fix: update proxy pass headers

--- nginx-le-setup.sh
+++ nginx-le-setup.sh
@@ -296,10 +296,11 @@ _create() {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \"upgrade\";
         proxy_set_header Host \$host;
+        proxy_set_header Forwarded \$proxy_add_forwarded;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto http;
-        proxy_set_header X-Nginx-Proxy true;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Scheme \$scheme;
         proxy_redirect off;
     }"
   fi
